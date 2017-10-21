f12d08d11b5cd0e2d82d04db9ad9d2e7016a393d
vance7 <pyang926@gmail.com>
2017-10-20 22:43:45 -0700

    Initial commit

---
+++ src/main/java/com/sharefun/sharefun/SharefunApplication.java
@@ -0,0 +1,12 @@
+package com.sharefun.sharefun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharefunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharefunApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sharefun/sharefun/SharefunApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sharefun.sharefun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SharefunApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
